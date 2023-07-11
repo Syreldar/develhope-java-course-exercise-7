acb0b1739a2db65c9d712304aadc78af5c7d374e
Syreldar <enrico.drago3@gmail.com>
2023-07-11 13:22:28 +0200

    Upload exercise

--- IfElseStatement.java
+++ IfElseStatement.java
@@ -2,5 +2,10 @@ public class IfElseStatement
 {
     public static void main(String[] args)
     {
+        String myName = "Enrico";
+        if (myName.length() % 2 == 0)
+            System.out.println("The number of letters in my name is even.");
+        else
+            System.out.println("The number of letters in my name is odd.");
     }
 }
